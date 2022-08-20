ed3bf445815889656b83e9c53c7bcc572d76ebbe
Rikfred <111575446+Rikfred@users.noreply.github.com>
2022-08-21 01:14:40 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,31 @@
+input.onButtonPressed(Button.A, function () {
+    if (liv > 0) {
+        paus = 0
+        for (let index = 0; index < liv; index++) {
+            basic.clearScreen()
+            basic.showIcon(IconNames.Heart)
+            basic.clearScreen()
+            basic.pause(400)
+        }
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    if (paus == 0) {
+        paus = 1
+        if (liv > 0) {
+            liv += -1
+        }
+        if (liv == 0) {
+            basic.showIcon(IconNames.Skull)
+        } else {
+            basic.showIcon(IconNames.No)
+        }
+    }
+})
+let paus = 0
+let liv = 0
+liv = 3
+paus = 1
 basic.forever(function () {
 	
 })
